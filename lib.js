f8e86951a74bc4fd6ddb3f39b52c086539630531
Samuel King <samuelmarquiceking@gmail.com>
2017-06-28 22:47:14 -0500

    added my curry assignment

--- lib.js
+++ lib.js
@@ -19,14 +19,31 @@ const listedPrice =
         ? listing.price
         : 0
 
+const total =
+(listings, items) => {
+  let result = 0
+  items.forEach(item => {
+    listings.forEach(listing => {
+      const priceLookup = listedPrice(listing)
+      result += priceLookup(item)
+    })
+  })
+  return result
+}
+
 /**
  * transform carts into an array of { customer, total }
  */
-const calculateTotals =
-  listings =>
-    carts => {
-      // TODO
-    }
+ const calculateTotals =
+   listings =>
+     carts => {
+       let result = []
+       carts.forEach(cart => {
+         const {customer, items} = cart
+         result.push({customer: customer, total: total(listings, items)})
+       })
+       return result
+     }
 
 module.exports = {
   listing,
